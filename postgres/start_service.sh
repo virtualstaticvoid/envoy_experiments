d926bbf96ad80cce9d15d790abd69297836fae84
Chris Stefano <virtualstaticvoid@gmail.com>
2018-06-26 11:08:38 +0100

    clean up services ~ remove direct dependency on envoy

--- postgres/start_service.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-set -e
-
-# use original image entrypoint + envoy sidecar
-docker-entrypoint.sh postgres &
-	envoy -c /etc/envoy.yaml --v2-config-only --service-cluster service${SERVICE_NAME}
--- rabbitmq/start_service.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-set -e
-
-# use original image entrypoint + envoy sidecar
-docker-entrypoint.sh rabbitmq-server &
-	envoy -c /etc/envoy.yaml --v2-config-only --service-cluster service${SERVICE_NAME}
